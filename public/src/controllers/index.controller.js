be2f493a76d5bf03a8ffd8c892b148b58f759dcc
Vlad Bezden <vlad.bezden@gmail.com>
2015-11-26 14:52:52 -0500

    Added validation if security already exists in the list, and if it exists, then it will not be added to the list

--- public/src/controllers/index.controller.js
+++ public/src/controllers/index.controller.js
@@ -21,12 +21,21 @@
 		
 		vm.securitySelected = function (item, model, label) {
 			$log.debug(item);
-			vm.selectedSecurities.push(item);
+			// check if that security is already in the list
+			if (!vm.selectedSecurities.some(checkBySymbol, item)) {
+				$log.debug('"' + item.Symbol + '" does not exist in the selected list and will be added to the list');
+				vm.selectedSecurities.push(item);	
+			}
+			
 			vm.selectedSecurity = undefined;
-		};
+		};		
 		
 		vm.deleteSecurity = function (index) {
 			vm.selectedSecurities.splice(index, 1);
 		};
+		
+		function checkBySymbol(security) {
+			return this.Symbol === security.Symbol;
+		}
 	}
 } (angular.module('app')));
